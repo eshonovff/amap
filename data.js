5553e9048fdb80219c31f46db94e0d86074aa101
Faridun Eshonov <fariduneshon02@gmail.com>
2024-05-09 11:46:07 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,22 @@
+// function halloween(dat) {
+//     if (dat.getMonth() == 9 && dat.getDate() == 31) return 'Bonfire toffee'
+
+//     return 'toffee'
+// }
+// let dat = new Date("2012/07/31")
+// console.log(halloween(dat));
+
+
+
+// function monthName(num) {
+//     const months = [
+//         "January", "February", "March", "April", "May", "June",
+//         "July", "August", "September", "October", "November", "December"
+//     ];
+
+
+//     return months[num - 1]
+
+
+// }
+// console.log(monthName(4));
